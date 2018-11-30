f7c63beafd7c657d9b2ae49c7abe0e24b3760447
Travis Lang <tlang50@icloud.com>
2018-11-30 16:07:17 -0600

    add buttons and icons

--- src/App.js
+++ src/App.js
@@ -1,26 +1,36 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Button from '@material-ui/core/Button';
+import 'typeface-roboto';
+
+import MuiThemeProvider from '@material-ui/core/styles/MuiThemeProvider';
+import { createMuiTheme } from '@material-ui/core/styles';
+import green from '@material-ui/core/colors/green';
+import { cyan } from '@material-ui/core/colors';
+
+import { AttachMoneyOutlined} from '@material-ui/icons';
+
+const theme = createMuiTheme({
+    palette: {
+        primary: green,
+        secondary: cyan
+    }
+});
+
+
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+        <MuiThemeProvider theme={theme}>
+            <div className="App">
+                <header className="App-header">
+                    <Button variant="outlined" color="primary">< AttachMoneyOutlined/></Button>
+                    <Button color="secondary">Stay</Button>
+                </header>
+            </div>
+        </MuiThemeProvider>
     );
   }
 }
